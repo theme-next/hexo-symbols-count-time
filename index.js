9ce209a24a7388f7f8d29bd550265d83c51a2af2
StevenJoeZhang <1119186082@qq.com>
2019-08-19 12:34:56 +0800

    Remove line breaks

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 
 if (hexo.config.symbols_count_time) {
 
-  var helper = require('./lib/helper');
+  const helper = require('./lib/helper');
+  const stripHTML = require('hexo-util').stripHTML;
 
   if (hexo.config.symbols_count_time.symbols) {
     hexo.extend.helper.register('symbolsCount', helper.symbolsCount);
@@ -22,11 +23,9 @@ if (hexo.config.symbols_count_time) {
 
   if (hexo.config.symbols_count_time.symbols || hexo.config.symbols_count_time.time || hexo.config.symbols_count_time.total_symbols || hexo.config.symbols_count_time.total_time) {
     hexo.extend.filter.register('after_post_render', function(data) {
-      var util = require('hexo-util');
-      var stripHTML = util.stripHTML;
-      var content = data.content.replace(/<td class=\"gutter\">.*?<\/td>/g, "");
-      if (hexo.config.symbols_count_time.exclude_codeblock) content = content.replace(/<td class=\"code\">.*?<\/td>/g, "");
-      data.length = stripHTML(content).length;
+      var content = data.content;
+      if (hexo.config.symbols_count_time.exclude_codeblock) content = content.replace(/<pre>.*?<\/pre>/g, '');
+      data.length = stripHTML(content).replace(/\n/g, '').length;
     }, 0);
   }
 
